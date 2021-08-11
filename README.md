6cd4f19b7a2998392631c7c63753a5975228b725
NP King <nathan@betapoint.co>
2021-08-11 19:13:15 -0400

    initial work breaking up the form

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Multi-step Form Tutorial
 
 In this project I show an example on how to build a multi-step form with react-hook-form
+
+watching here:
+https://www.youtube.com/watch?v=Wlz2cy33bMU&ab_channel=AustinShelby
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,38 @@
 import React from "react"
 
 const IndexPage = () => {
+
+  const [formStep, setFormStep] = React.useState(0)
+
+  const completeFormStep = ()  => {
+    setFormStep(cur => cur+1)
+  }
+
+  const renderButton = () => {
+    if(formStep > 2) {
+      return undefined;
+    } else if (formStep == 2) {
+      return (
+        <button
+          onClick={completeFormStep}
+          type="button"
+          className="mt-6 bg-green-600 text-white rounded px-8 py-6 w-full disabled:bg-gray-400 disabled:cursor-not-allowed"
+        >
+          YARRRRR
+        </button>
+        )
+    } else {
+            return (
+            <button
+              onClick={completeFormStep}
+              type="button"
+              className="mt-6 bg-green-600 text-white rounded px-8 py-6 w-full disabled:bg-gray-400 disabled:cursor-not-allowed"
+            >
+              Next Step
+            </button>
+            )
+    }
+  }
   return (
     <div className="min-h-screen bg-green-900 flex flex-col items-start text-gray-900 antialiased relative">
       <div
@@ -21,25 +53,39 @@ const IndexPage = () => {
       <div className="max-w-xl w-full mt-24 mb-24 rounded-lg shadow-2xl bg-white mx-auto overflow-hidden z-10">
         <div className="px-16 py-10">
           <form>
-            <h2 className="font-semibold text-3xl mb-8">
-              Personal Information
-            </h2>
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              className="text-input"
-            />
-            <h2 className="font-semibold text-3xl mb-8">Billing Information</h2>
-            <label htmlFor="address">Address</label>
-            <input
-              type="text"
-              id="address"
-              name="address"
-              className="text-input"
-            />
-            <h2 className="font-semibold text-3xl mb-8">Legal Information</h2>
+            
+
+            {/* SECTION 1 */}
+            {formStep === 0 && (<section>
+              <h2 className="font-semibold text-3xl mb-8">
+                Personal Information
+              </h2>
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                className="text-input"
+              />
+            </section>)}
+
+
+            {/* SECTION 2 */}
+            {formStep === 1 && (<section>
+              <h2 className="font-semibold text-3xl mb-8">Billing Information</h2>
+              <label htmlFor="address">Address</label>
+              <input
+                type="text"
+                id="address"
+                name="address"
+                className="text-input"
+              />
+            </section>)}
+
+
+            {/* SECTION 3 */}
+            {formStep === 2 && (<section>
+              <h2 className="font-semibold text-3xl mb-8">Legal Information</h2>
             <div className="block mt-6">
               <input
                 name="toc"
@@ -67,13 +113,15 @@ const IndexPage = () => {
                 </a>
                 .
               </span>
-            </div>
-            <button
-              type="submit"
-              className="mt-6 bg-green-600 text-white rounded px-8 py-6 w-full disabled:bg-gray-400 disabled:cursor-not-allowed"
-            >
-              Create Account
-            </button>
+              </div>
+            </section>)}
+
+            {/* SECTION 4 */}
+            {formStep === 3 && (<section>
+              <h1 className="font-semibold text-3xl mb-8">Congratulations !!</h1>
+              </section>)}
+              {/* SUBMIT BUTTON FOR THE FORM */}
+              {renderButton()}
           </form>
         </div>
       </div>
